c4489c82bc30307208499f7a17aa30ce59697651
snapboss <mbajrami95@gmail.com>
2025-05-02 12:55:45 +0200

    duchamp: Include thermal files for GPU Fix * we blame xiaomi for this
    Signed-off-by: snapboss <mbajrami95@gmail.com>
    Signed-off-by: Saikrishna1504 <saikrishna26918@gmail.com>

--- device.mk
+++ device.mk
@@ -348,6 +348,10 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/thermals/,$(TARGET_COPY_OUT_ODM)/etc)
 
+# Thermal files
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(DEVICE_PATH)/configs/thermals/,$(TARGET_COPY_OUT_ODM)/etc)
+
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal-service.pixel \
