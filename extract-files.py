6ea79083f8565a12fcb1f2988de951477a478aad
ShadowSparkle <shadowproxdxd@hotmail.com>
2026-03-30 00:27:49 +0000

    duchamp: Import system NPU libs

--- extract-files.py
+++ extract-files.py
@@ -32,6 +32,8 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     ('libmialgo_aio_seg',
      'libmialgo_utils',
+     'vendor.mediatek.hardware.apuware.utils-V1-ndk',
+     'vendor.mediatek.hardware.apuware.utils@2.0',
      'vendor.mediatek.hardware.videotelephony-V1-ndk',
      'vendor.mediatek.hardware.camera.isphal@1.0',
      'vendor.mediatek.hardware.camera.isphal-V1-ndk',): lib_fixup_vendor_suffix,
